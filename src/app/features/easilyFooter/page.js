6f33deaa427ff5000649ce93468a98edac75122e
dohee12 <happyddo42@gmail.com>
2025-01-06 17:20:40 +0900

    feat: 공통 footer 페이지 구현 완료

---
+++ src/app/features/easilyFooter/page.js
@@ -0,0 +1,29 @@
+import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Footer() {
+    return (
+      <div className="w-full p-10 h-auto bg-black text-white">
+        <h2 className="text-2xl font-bold mb-6">JOJI COMPANY</h2>
+        <p className="text-lg text-muted-foreground">
+            대표자: 조영진<br/>
+            주소 : 경기도 안양시 시민대327번길 11-41306호<br/>
+            contact mail : contact@jojicompany.com<br/>
+            contact phone : 010-7510-7298
+        </p><br/>
+        <a href="#" className="text-lg text-muted-foreground">
+            서비스 약관
+        </a><br/>
+        <a href="#" className="text-lg text-muted-foreground">
+            개인정보 이용약관
+        </a><br/>
+        <a href="https://www.youtube.com" className="text-lg text-muted-foreground pr-2">
+            <FontAwesomeIcon icon={faYoutube} size="2x"/>
+        </a>
+        <a href="https://www.instagram.com" className="text-lg text-muted-foreground">
+            <FontAwesomeIcon icon={faInstagram} size="2x"/>
+        </a>
+      </div>
+    );
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import Contact from "./features/easilyContact/page";
 import Hero from "./features/easilyMain/page";
 import Service from "./features/easilyService/page";
-
+import Footer from "./features/easilyFooter/page";
 
 export default function Home() {
   return (
@@ -21,6 +21,11 @@ export default function Home() {
       {/* Contact Section */}
       <section id="contact" className="min-h-screen flex items-center justify-center bg-gray-100">
         <Contact />
+      </section>
+
+      {/* Footer Section */}
+      <section id="footer" className="min-h-screen flex items-center justify-center bg-gray-50">
+        <Footer />
       </section>
     </div>
     </body>
