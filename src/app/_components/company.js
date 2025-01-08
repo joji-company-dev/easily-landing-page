7090b1a37b5988d2ecd9cdf791994b75b3a4d57b
dohee12 <happyddo42@gmail.com>
2025-01-08 10:18:10 +0900

    feat: footer Company 정보 모두 company.js 파일로 이동 및 약관 색상 변경

--- src/app/_components/company.js
+++ src/app/_components/company.js
@@ -1,7 +1,11 @@
 export const COMPANY_INFO = {
-    name: "JOJI COMPANY",
-    representative: "조영진",
-    address: "경기도 안양시 시민대327번길 11-41306호",
-    email: "contact@jojicompany.com",
-    phone: "010-7510-7298"
+    Name: "JOJI COMPANY",
+    Representative: "조영진",
+    Address: "경기도 안양시 시민대327번길 11-41306호",
+    Email: "contact@jojicompany.com",
+    Tel: "010-7510-7298",
+    Service: "#",
+    Userule: "#",
+    Youtube: "#",
+    Instagram: "#"
   }
