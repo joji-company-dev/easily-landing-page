caac95ba8137485449d354fcbf38930e06823a48
JEONGWOON KIM ( MOOHO KIM ) <75013334+AlangGY@users.noreply.github.com>
2025-01-14 15:16:55 +0900

    config: 로컬 개발 환경을 위해, /api 경로에 대해서 easily-api로 proxy 처리 (#16)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "https://easily-api.jojicompany.com/:path*",
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
+};
 
 export default nextConfig;
