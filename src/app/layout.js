01f8b1a6dcf5cc9c2ca4efc01438e81a76639c50
dohee12 <happyddo42@gmail.com>
2025-01-15 15:45:53 +0900

    feat: 파비콘 적용 및 metadata의 title, description 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
+import { FileArchiveIcon, Link } from "lucide-react";
 import NavBar from "./_features/navBar";
 import Footer from "./_features/sections/footerSection";
 import "./globals.css";
 
 
 export const metadata = {
-  title: "이즐리 렌딩페이지",
-  description: "이즐리 렌딩페이지 입니다. ",
+  title:"이즐리 | 영상 기획안",
+  description:"세상 쉬운 영상기획 이젠 이즐리로 하세요 · 프로턱션을 위한 최고의 도구 · 창의적인 일에 집중하세요 · 간편한 기획안 초안 생성 · 고품질 레퍼런스 이미지 생성",
 };
 
 export default function RootLayout({ children }) {
