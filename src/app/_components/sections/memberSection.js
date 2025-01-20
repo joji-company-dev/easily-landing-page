c94e1ea3a316b568d4be9c5fdfa677b23d9d5680
dohee12 <happyddo42@gmail.com>
2025-01-20 17:04:42 +0900

    feat: flex 대신 grid로 변경

--- src/app/_components/sections/memberSection.js
+++ src/app/_components/sections/memberSection.js
@@ -5,12 +5,14 @@ export function MemberSection() {
     return (
         <div className="w-full">
             <h1 className="font-black text-center text-3xl m-5 font-sans">많은 고객에게 사랑받고있어요</h1>
-            {MemberList.map((MemberList) => (
-                <Card key={MemberList.id} className="item-center m-6 flex flex-col gap-4">
-                    <CardContent className="font-bold p-3 text-base rounded-t-lg shadow-sm font-sans">{MemberList.content}</CardContent>
-                    <CardFooter className="font-extrabold p-3 text-sm rounded-b-lg shadow-md font-sans">{MemberList.member}</CardFooter>
-                </Card>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6 item-center">
+                {MemberList.map((MemberList) => (
+                    <Card key={MemberList.id} className="w-[240px] h-[320px]">
+                        <CardContent className="font-bold p-3 text-base rounded-t-lg shadow-sm font-sans">{MemberList.content}</CardContent>
+                        <CardFooter className="font-extrabold p-3 text-sm rounded-b-lg shadow-md font-sans">{MemberList.member}</CardFooter>
+                    </Card>
             ))}
+            </div>
         </div>
     )
 }
