0fc31b623153f25d1dbcfbad4f4dadcdbbccbf9e
MuteAim <ji_hyunsoo@naver.com>
2025-01-21 11:38:30 +0900

    config: 플젝내에 husky, eslint, prettier 설정
    - 작업물을 저장하게 되면 자동으로 명시한 프리티어 규칙에 맞게 자동정렬해줌

--- src/app/page.js
+++ src/app/page.js
@@ -9,19 +9,34 @@ import { SubscribeSection } from "./_components/sections/subscribeSection";
 export default function Home() {
   return (
     <div>
-      <section id="hero" className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section
+        id="hero"
+        className="min-h-screen flex items-center justify-center bg-gray-100"
+      >
         <HeroSection />
       </section>
-      <section id="service" className="min-h-screen flex items-center justify-center bg-white">
+      <section
+        id="service"
+        className="min-h-screen flex items-center justify-center bg-white"
+      >
         <ServiceSection />
       </section>
-      <section id="faq" className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section
+        id="faq"
+        className="min-h-screen flex items-center justify-center bg-gray-100"
+      >
         <FAQSection />
       </section>
-      <section id="subscribe" className="min-h-screen flex items-center justify-center bg-white">
+      <section
+        id="subscribe"
+        className="min-h-screen flex items-center justify-center bg-white"
+      >
         <SubscribeSection />
       </section>
-      <section id="contact" className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section
+        id="contact"
+        className="min-h-screen flex items-center justify-center bg-gray-100"
+      >
         <ContactSection />
       </section>
     </div>
