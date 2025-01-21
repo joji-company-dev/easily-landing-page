8270d419b3bfb1793f17564fa9db251c0d026423
dohee12 <happyddo42@gmail.com>
2025-01-21 11:45:28 +0900

    feat: Member, FAQ Section  높이 변경경

--- src/app/page.js
+++ src/app/page.js
@@ -22,11 +22,11 @@ export default function Home() {
         <ContactSection />
       </section>
       {/* Member Section */}
-      <section id="member" className="h-[500px] flex items-center justify-center bg-gray-100">
+      <section id="member" className="h-1/2 flex items-center justify-center bg-gray-100">
         <MemberSection/>
       </section>
       {/* FAQ Section */}
-      <section id="faq" className="h-[500px] flex items-center justify-center bg-gray-100">
+      <section id="faq" className="h-1/2 flex items-center justify-center bg-gray-100">
         <FAQSection />
       </section>
     </div>
