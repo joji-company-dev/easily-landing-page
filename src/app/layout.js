aaf9b5f1466181d1e575bbcdf93ca64ffc665272
Hyun <151635939+MuteAim@users.noreply.github.com>
2025-01-21 15:57:24 +0900

    [JOJI-734] 프로젝트 prettier설정 (#22)
    * feat: 허스키 기본설정
    
    * config: 플젝내에 husky, eslint, prettier 설정
    
    - 작업물을 저장하게 되면 자동으로 명시한 프리티어 규칙에 맞게 자동정렬해줌
    
    * feat: package-lock.json 업데이트

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,20 +3,20 @@ import Footer from "./_components/footer";
 import "./globals.css";
 import { Toaster } from "./_components/ui/toaster";
 
-
 export const metadata = {
-  title:"이즐리 | 영상 기획안",
-  description:"세상 쉬운 영상기획 이젠 이즐리로 하세요 · 프로턱션을 위한 최고의 도구 · 창의적인 일에 집중하세요 · 간편한 기획안 초안 생성 · 고품질 레퍼런스 이미지 생성",
+  title: "이즐리 | 영상 기획안",
+  description:
+    "세상 쉬운 영상기획 이젠 이즐리로 하세요 · 프로턱션을 위한 최고의 도구 · 창의적인 일에 집중하세요 · 간편한 기획안 초안 생성 · 고품질 레퍼런스 이미지 생성",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-      <NavBar/>
-      {children}
-      <Toaster/>
-      <Footer/>
+        <NavBar />
+        {children}
+        <Toaster />
+        <Footer />
       </body>
     </html>
   );
