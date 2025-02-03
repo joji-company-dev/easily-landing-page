b0ff9c3c1c06f00682177a2f5010b62d39883a7e
dohee <155977634+dohee12@users.noreply.github.com>
2025-02-03 10:50:09 +0900

    [JOJI-729] 사용자 후기 섹션 (#23)
    * feat: 사용자 후기 섹션 구현현
    
    * feat: 사용자 후기 섹션 page에 추가
    
    * feat: Member, FAQ 섹션 높이 변경경
    
    * feat: flex 대신 grid로 변경
    
    * feat:  Home의 Member Section, FAQ Section 높이 변경경
    
    * feat: Member, FAQ Section  높이 변경경
    
    * feat:   카드 사이즈 변경 및 border 추가
    
    * feat: Separator 사용하여 후기 섹션 구현현
    
    * feat: skeleton으로 후기 섹션 구현
    
    * feat: Resizable으로 후기 섹션 구현
    
    * config:  member 관련 파일 이름 변경
    
    * feat: 사용한 jsx 파일일
    
    * feat: riew 파일명 수정 및 alert 파일일
    
    * member 관련 파일 삭제 (이제 사용 안함)
    
    * feat: Alert으로 후기 섹션 구현
    
    * fix: package-lock-json 변경경
    
    * feat: map 함수를 사용하는 const들을 모아 파일 생성 및 선언언
    
    * feat: 수많던 주석을 없애고 Card ui를 import 하여 사용자 후기 섹션 구현 완료료
    
    * feat: review_list 파일명 변경 및 변수명 변경경
    
    * feat: package-lock.json 재설치
    
    * feat: _components에 있던 const 다른 곳으로  이동동

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { HeroSection } from "./_components/sections/heroSection";
 import { ContactSection } from "./_components/sections/contactSection";
 import { ServiceSection } from "./_components/sections/serviceSection";
 import { FAQSection } from "./_components/sections/faqSection";
+import { ReviewSection } from "./_components/sections/reviewSection";
 import { SubscribeSection } from "./_components/sections/subscribeSection";
 
 export default function Home() {
@@ -21,10 +22,16 @@ export default function Home() {
       >
         <ServiceSection />
       </section>
-      <section
-        id="faq"
-        className="min-h-screen flex items-center justify-center bg-gray-100"
-      >
+      {/* Contact Section */}
+      <section id="contact" className="min-h-screen flex items-center justify-center bg-gray-100">
+        <ContactSection />
+      </section>
+      {/* Review Section */}
+      <section id="review" className="h-1/2 flex items-center justify-center bg-gray-100">
+        <ReviewSection/>
+      </section>
+      {/* FAQ Section */}
+      <section id="faq" className="h-1/2 flex items-center justify-center bg-gray-100">
         <FAQSection />
       </section>
       <section
