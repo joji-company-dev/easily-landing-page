930638ef214d9b05477cc747d6c81a1bb9f80e4c
Hyunsoo <151635939+MuteAim@users.noreply.github.com>
2025-02-03 11:19:53 +0900

    [JOJI-727] 사용자 드롭다운 메뉴 구현 (#24)
    * feat: 네비게이션 바 업데이트
    
    - 가상의 로그인 상태를 만들어서
    로그인 여부에 따른 네비게이션 상태 변화
    
    * feat: easilyPosts 수정
    
    - 취지에 맞게 NOTICE만 보여주도록 변경
    
    * feat: 드롭다운 수정
    
    -메뉴와 사용자 드롭다운을 분리하여 동시에 드롭다운이 되는 현상 개선
    
    * feat: 로그인상태에 따른 네비게이션 바(로그인 처리)
    
    - 쿠키에 엑세스토큰이 존재할경우 Welcome, 존재하지 않을경우에는 로그인버튼이 표시됨
    
    * feat: 로그인 상태 개선
    
    현재 세가지의 상태가 존재합니다.
    1. 로그인이 되어있는 상태 -> Welcome
    2. 로그인 시도중인 상태 ->
    아무것도 나오지 않음
    3. 로그인이 안되어있는 상태 ->
    로그인 버튼 표시
    
    * feat: easilyPosts 개선

--- src/app/_components/navBar.js
+++ src/app/_components/navBar.js
@@ -1,10 +1,61 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMenuDropdownOpen, setIsMenuDropdownOpen] = useState(false); // 메뉴 드롭다운 상태
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false); // 사용자메뉴 드롭다운 상태
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 여부 상태
+  const [isLoading, setIsLoading] = useState(true); //로그인 시도중인 상태
+  const [userName, setUserName] = useState(""); // 사용자 이름
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/profile`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 401) {
+          throw new Error("Unauthorized");
+        } else {
+          throw new Error("Unexpected error");
+        }
+      })
+      .then((data) => {
+        setIsLoggedIn(true);
+        setUserName(data.name);
+      })
+      .catch((error) => {
+        if (error.message === "Unauthorized") {
+          setIsLoggedIn(false);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  //로그아웃 기능 미완성
+  const handleLogout = () => {
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+    }).then((response) => {
+      if (response.status === 201) {
+        setIsLoggedIn(false);
+        setIsUserDropdownOpen(false);
+      } else {
+        console.error("Failed to logout.");
+      }
+    });
+  };
+
+  const redirectToLogin = () => {
+    window.location.href = `https://easily-dashboard.jojicompany.com/login?fallback=${window.location.href}`;
+  };
 
   const menuItems = [
     {
@@ -13,7 +64,9 @@ export default function NavBar() {
       children: [
         { label: "소개", href: "#hero" },
         { label: "서비스", href: "#service" },
-        { label: "문의/구독", href: "#contact" },
+        { label: "FAQ", href: "#faq" },
+        { label: "구독", href: "#subscribe" },
+        { label: "문의", href: "#contact" },
       ],
     },
     {
@@ -21,7 +74,7 @@ export default function NavBar() {
       baseUrl: "/",
       children: [
         { label: "정책", href: "#policy" },
-        { label: "결제", href: "#purcahse" },
+        { label: "결제", href: "#purchase" },
       ],
     },
     {
@@ -37,10 +90,10 @@ export default function NavBar() {
   return (
     <nav
       className="border-b sticky bg-white top-0 z-50"
-      onMouseLeave={() => setIsDropdownOpen(false)}
+      onMouseLeave={() => setIsMenuDropdownOpen(false)}
     >
       <div className="flex items-center justify-between px-6 py-4 max-w-7xl mx-auto">
-        {/* Left: Logo */}
+        {/* 로고 */}
         <Link href="/" className="flex items-center">
           <Image
             src="/logo(2).svg"
@@ -51,10 +104,10 @@ export default function NavBar() {
           />
         </Link>
 
-        {/* Center: Menu Items */}
+        {/* 메뉴 */}
         <div
-          className="relative flex items-center gap-28"
-          onMouseEnter={() => setIsDropdownOpen(true)}
+          className="relative flex items-center gap-28 flex-1 justify-center"
+          onMouseEnter={() => setIsMenuDropdownOpen(true)}
         >
           {menuItems.map((menu, index) => (
             <div key={index} className="relative">
@@ -67,11 +120,11 @@ export default function NavBar() {
           ))}
 
           {/* 드롭다운 전체 메뉴 */}
-          {isDropdownOpen && (
+          {isMenuDropdownOpen && (
             <div
               className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white shadow-md rounded-b-lg p-4 w-[550px] mt-2"
-              onMouseEnter={() => setIsDropdownOpen(true)}
-              onMouseLeave={() => setIsDropdownOpen(false)}
+              onMouseEnter={() => setIsMenuDropdownOpen(true)}
+              onMouseLeave={() => setIsMenuDropdownOpen(false)}
             >
               <div className="flex justify-around gap-2">
                 {menuItems.map((menu, index) => (
@@ -92,18 +145,52 @@ export default function NavBar() {
           )}
         </div>
 
-        {/* Right: Dashboard and Login Buttons */}
         <div className="flex items-center gap-4">
-          <Link href="/dashboard">
+          <Link href="https://easily-dashboard.jojicompany.com">
             <button className="bg-[#FF6B2B] text-white py-2 px-4 rounded-md hover:bg-[#e55a1f]">
               대시보드
             </button>
           </Link>
-          <Link href="/login">
-            <button className="bg-gray-300 text-black py-2 px-4 rounded-md hover:bg-gray-400">
-              로그인
-            </button>
-          </Link>
+
+          {/* 로그인 여부에 따른 사용자 정보 */}
+          <div className="relative w-40">
+            {isLoading ? null : isLoggedIn ? (
+              <div>
+                <button
+                  className="text-sm font-semibold text-muted-foreground"
+                  onMouseEnter={() => setIsUserDropdownOpen((prev) => !prev)}
+                >
+                  환영합니다! {userName}님
+                </button>
+                {isUserDropdownOpen && (
+                  <div
+                    className="absolute right-0 mt-2 bg-white shadow-md rounded-lg py-2 w-40"
+                    onMouseLeave={() => setIsUserDropdownOpen(false)}
+                  >
+                    <Link
+                      href="/myinfo"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      내 정보
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      로그아웃
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <button
+                className="bg-gray-300 text-black py-2 px-4 rounded-md hover:bg-gray-400"
+                onClick={redirectToLogin}
+              >
+                로그인
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </nav>
