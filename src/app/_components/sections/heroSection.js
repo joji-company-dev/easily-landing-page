631fead23ec7e954940e4b31a6db57a96171ac0c
MuteAim <ji_hyunsoo@naver.com>
2025-02-04 17:41:36 +0900

    feat: 소개세션 반응형 디자인

--- src/app/_components/sections/heroSection.js
+++ src/app/_components/sections/heroSection.js
@@ -8,23 +8,33 @@ import EasilyPosts from "@/app/_components/easilyPosts";
 
 export function HeroSection() {
   return (
-    <div className="px-6 py-16 sm:py-24">
-      <div className="flex flex-col md:flex-row items-start justify-between gap-52">
-        {/* 왼쪽 콘텐츠*/}
-        <div className="flex flex-col gap-12 w-full">
+    <div className="px-6 py-12 sm:py-16 md:py-24">
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-10 md:gap-20 lg:gap-32">
+        <div className="w-full justify-center flex md:hidden">
+          <Card className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[30rem] lg:h-[30rem]">
+            <Image
+              src="/logo(1).svg"
+              alt="Easily Beta"
+              fill
+              className="object-contain p-6"
+            />
+          </Card>
+        </div>
+        {/* 왼쪽 콘텐츠 */}
+        <div className="flex flex-col gap-8 md:gap-12 w-full">
           <div className="text-center md:text-left">
-            <h1 className="sm:text-8xl font-bold leading-tight mb-6 tracking-tight">
+            <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-tight mb-6 tracking-tight">
               세상 쉬운 영상기획
               <br />
               이젠 <span className="text-primary">이즐리</span>로 하세요
             </h1>
-            <p className="text-lg sm:text-xl text-muted-foreground mb-8">
+            <p className="text-base sm:text-lg md:text-xl text-muted-foreground mb-6 md:mb-8">
               이즐리는 영상기획툴을 제공합니다
             </p>
             <Button
               asChild
               size="lg"
-              className="bg-primary text-white hover:bg-[#e55a1f] px-8 py-4 text-lg sm:text-xl"
+              className="bg-primary text-white hover:bg-[#e55a1f] px-6 sm:px-8 py-3 sm:py-4 text-base sm:text-lg md:text-xl"
             >
               <Link href="https://easily-dashboard.jojicompany.com/dashboard/proposal/create">
                 기획안 만들기
@@ -36,9 +46,9 @@ export function HeroSection() {
           <EasilyPosts />
         </div>
 
-        {/* 오른쪽 콘텐츠*/}
-        <div className="w-full flex justify-center">
-          <Card className="relative w-96 h-96 sm:w-[30rem] sm:h-[30rem]">
+        {/* 오른쪽 콘텐츠 */}
+        <div className="w-full justify-center hidden md:flex">
+          <Card className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[30rem] lg:h-[30rem]">
             <Image
               src="/logo(1).svg"
               alt="Easily Beta"
