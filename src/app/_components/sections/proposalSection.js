fa6094a0f6ef5906b0d813af2039bb9799aff4f4
dohee12 <happyddo42@gmail.com>
2025-02-11 15:30:59 +0900

    feat: Carousel Previous, Next 버튼 대칭 수정 및 사이즈 변경경

--- src/app/_components/sections/proposalSection.js
+++ src/app/_components/sections/proposalSection.js
@@ -6,46 +6,39 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../ui/carousel";
-import * as React from "react";
-import Autoplay from "embla-carousel-autoplay";
 import { PROPOSAL_LIST } from "@/app/_consts/proposal_list";
 
 export function ProposalSection() {
-  const plugin = React.useRef(
-    Autoplay({ delay: 1000, stopOnInteraction: true })
-  );
   return (
     <div>
-      <Carousel
-        plugins={[plugin.current]}
-        className="w-full max-w-xl"
-        onMouseEnter={plugin.current.stop}
-        onMouseLeave={plugin.current.reset}
-      >
+      <h1 className="font-black text-center text-6xl m-10 font-serif">
+        Heading
+      </h1>
+      <Carousel className="w-full max-w-screen-sm p-5">
         <CarouselContent>
           {PROPOSAL_LIST.map((proposal) => (
             <CarouselItem key={proposal.id}>
               <div className="p-1">
                 <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-10">
-                    <img
-                      src={proposal.image}
-                      alt={proposal.id}
-                      className="text-4xl font-semibold"
-                    />
+                  <CardContent className="flex aspect-square items-center justify-center">
+                    <img src={proposal.image} alt={proposal.id} />
                   </CardContent>
                 </Card>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <CarouselPrevious className="w-20 h-20 -m-10" />
+        <CarouselNext className="w-20 h-20 -m-10" />
       </Carousel>
-      <h1 className="font-black text-center text-4xl m-10 font-sans">
-        생성된 기획안 수
-      </h1>
-      <div></div>
+      <div className="w-full max-w-screen-sm">
+        <h1 className="font-black text-center text-6xl m-10 font-sans">
+          생성된 기획안 수
+        </h1>
+        <p className="font-black text-center text-5xl m-10 font-serif">
+          10000000+
+        </p>
+      </div>
     </div>
   );
 }
