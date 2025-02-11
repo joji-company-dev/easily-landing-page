8316412f2ea50e153baeacc4c3b970ca955885b3
MuteAim <ji_hyunsoo@naver.com>
2025-02-11 15:23:28 +0900

    feat: 메뉴아이템 추출
    -serviceItem.js 로 컴포넌트분리

--- src/app/_components/sections/serviceSection.js
+++ src/app/_components/sections/serviceSection.js
@@ -1,28 +1,6 @@
-export function ServiceSection() {
-  const serviceItem = [
-    {
-      id: 1,
-      title: "서비스1",
-      subtitle: "이즐리는 여러분의 일상에 행복을 줍니다.",
-      videoSrc:
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-    },
-    {
-      id: 2,
-      title: "서비스2",
-      subtitle: "이즐리는 여러분의 일상에 행복을 줍니다.",
-      videoSrc:
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    },
-    {
-      id: 3,
-      title: "서비스3",
-      subtitle: "이즐리는 여러분의 일상에 행복을 줍니다.",
-      videoSrc:
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-    },
-  ];
+import { serviceItem } from "@/app/_consts/serviceItem";
 
+export function ServiceSection() {
   return (
     <div className="max-w-7xl mx-auto px-6 py-24 text-center bg-gradient-to-b text-black">
       <h2 className="text-5xl font-extrabold mb-12">✨ 이즐리 ✨</h2>
