3b3a213b5085fb47f24c055488ba5e8297d82f78
dohee12 <happyddo42@gmail.com>
2025-02-11 16:26:29 +0900

    feat: useState, useRef, useEffect, IntersectionObserver 사용하여 생성된 기획안 수에 대한 숫자 애니메이션 구현

--- src/app/_components/sections/proposalSection.js
+++ src/app/_components/sections/proposalSection.js
@@ -7,10 +7,58 @@ import {
   CarouselPrevious,
 } from "../ui/carousel";
 import { PROPOSAL_LIST } from "@/app/_consts/proposal_list";
+import { useEffect, useRef, useState } from "react";
 
 export function ProposalSection() {
+  const [count, setCount] = useState(0);
+  const sectionRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (observer && sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      const targetNum = 5000;
+      const duration = 6000;
+      const increment = targetNum / (duration / 30);
+
+      let currentNum = 0;
+      const animateCount = () => {
+        if (currentNum < targetNum) {
+          currentNum += increment;
+          setCount(Math.floor(currentNum));
+          requestAnimationFrame(animateCount);
+        } else {
+          setCount(targetNum);
+        }
+      };
+
+      animateCount();
+    }
+  }, [isVisible]);
+
   return (
-    <div>
+    <div ref={sectionRef}>
       <h1 className="font-black text-center text-6xl m-10 font-serif">
         Heading
       </h1>
@@ -21,7 +69,7 @@ export function ProposalSection() {
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center">
-                    <img src={proposal.image} alt={proposal.id} />
+                    <img src={proposal.image} alt={`${proposal.id}번 이미지`} />
                   </CardContent>
                 </Card>
               </div>
@@ -36,7 +84,7 @@ export function ProposalSection() {
           생성된 기획안 수
         </h1>
         <p className="font-black text-center text-5xl m-10 font-serif">
-          10000000+
+          {count.toLocaleString()}+
         </p>
       </div>
     </div>
