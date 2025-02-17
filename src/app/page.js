dba4ec66394a20b02ab32a69b578bd0b55506582
MuteAim <ji_hyunsoo@naver.com>
2025-02-17 15:00:15 +0900

    feat: 각 섹션들을 Detector로 감싸기

--- src/app/page.js
+++ src/app/page.js
@@ -5,40 +5,51 @@ import { ServiceSection } from "./_components/sections/serviceSection";
 import { FAQSection } from "./_components/sections/faqSection";
 import { ReviewSection } from "./_components/sections/reviewSection";
 import { SubscribeSection } from "./_components/sections/subscribeSection";
+import { Detector } from "./_components/common/detector";
 
 export default function Home() {
   return (
     <div>
-      <section
-        id="hero"
-        className="min-h-screen flex items-center justify-center bg-gray-100"
-      >
-        <HeroSection />
-      </section>
-      <section
-        id="service"
-        className="min-h-screen flex items-center justify-center bg-white"
-      >
-        <ServiceSection />
-      </section>
-      <section
-        id="review"
-        className="h-1/2 flex items-center justify-center bg-gray-100"
-      >
-        <ReviewSection />
-      </section>
-      <section
-        id="faq"
-        className="h-1/2 flex items-center justify-center bg-gray-100"
-      >
-        <FAQSection />
-      </section>
-      <section
-        id="subscribe"
-        className="min-h-screen flex items-center justify-center bg-white"
-      >
-        <SubscribeSection />
-      </section>
+      <Detector>
+        <section
+          id="hero"
+          className="min-h-screen flex items-center justify-center bg-gray-100"
+        >
+          <HeroSection />
+        </section>
+      </Detector>
+      <Detector>
+        <section
+          id="service"
+          className="min-h-screen flex items-center justify-center bg-white"
+        >
+          <ServiceSection />
+        </section>
+      </Detector>
+      <Detector>
+        <section
+          id="review"
+          className="h-1/2 flex items-center justify-center bg-gray-100"
+        >
+          <ReviewSection />
+        </section>
+      </Detector>
+      <Detector>
+        <section
+          id="faq"
+          className="h-1/2 flex items-center justify-center bg-gray-100"
+        >
+          <FAQSection />
+        </section>
+      </Detector>
+      <Detector>
+        <section
+          id="subscribe"
+          className="min-h-screen flex items-center justify-center bg-white"
+        >
+          <SubscribeSection />
+        </section>
+      </Detector>
     </div>
   );
 }
