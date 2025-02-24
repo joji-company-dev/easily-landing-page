a9c4f9de66b54d8cb7f7fb8c59b32b41b97f18ad
MuteAim <ji_hyunsoo@naver.com>
2025-02-24 11:30:44 +0900

    feat: heroSection UI개선

--- src/app/_components/sections/heroSection.js
+++ src/app/_components/sections/heroSection.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { TypingEffect } from "../ui/typingEffect";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 export function HeroSection() {
   const scrollToSection = () => {
@@ -19,9 +20,9 @@ export function HeroSection() {
         </div>
 
         <p className=" mt-5 text-white text-lg sm:text-4xl md:text-4xl font-medium opacity-90">
-          🎬 창의적인 영상 기획을{" "}
+          창의적인 영상 기획을{" "}
           <span className="text-point font-bold">
-            쉽게, 빠르게, 효과적으로!
+            쉽고, 빠르게, 효과적으로!
           </span>
         </p>
         <div className="flex sm:flex-row justify-center w-full gap-4 mt-6">
@@ -46,7 +47,7 @@ export function HeroSection() {
       </div>
       <div className="relative flex justify-between mt-20">
         <motion.div
-          className="p-2 text-4xl sm:text-5xl md:text-4xl text-white font-bold mt-4 flex items-center gap-2"
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 0.5 }}
@@ -56,13 +57,13 @@ export function HeroSection() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.7 }}
           >
-            ✅
+            <IoMdCheckmarkCircleOutline className="text-point" />
           </motion.span>
           AI사용
         </motion.div>
 
         <motion.div
-          className="p-2 text-4xl sm:text-5xl md:text-4xl text-white font-bold mt-4 flex items-center gap-2"
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 1 }}
@@ -72,13 +73,13 @@ export function HeroSection() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 1.2 }}
           >
-            ✅
+            <IoMdCheckmarkCircleOutline className="text-point" />
           </motion.span>
           고품질
         </motion.div>
 
         <motion.div
-          className="p-2 text-4xl sm:text-5xl md:text-4xl text-white font-bold mt-4 flex items-center gap-2"
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 1.5 }}
@@ -88,7 +89,7 @@ export function HeroSection() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 1.7 }}
           >
-            ✅
+            <IoMdCheckmarkCircleOutline className="text-point" />
           </motion.span>
           편리함
         </motion.div>
