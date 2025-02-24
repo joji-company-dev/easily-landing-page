0cd50bc947d7e375acd51cca60348ea4e50d061d
Hyunsoo <151635939+MuteAim@users.noreply.github.com>
2025-02-24 14:03:05 +0900

    [JOJI-845] 렌딩페이지 디자인 수정 (#39)
    * feat: hero섹션 애니메이션 적용
    
    * feat: hero섹션 1안
    
    * feat: 네비게이션 바 작동 수정
    
    - 공지사항 페이지로 이동시에 하이라이트가 반영되지 않는점 수정
    
    * feat: heroSection UI개선
    
    * feat: 포인트 컬러 변경
    
    * feat: 불필요한 ui 컴포넌트 삭제

--- src/app/_components/sections/heroSection.js
+++ src/app/_components/sections/heroSection.js
@@ -1,62 +1,115 @@
 "use client";
-
 import Image from "next/image";
-import Link from "next/link";
-import { Button } from "@/app/_components/ui/button";
-import { Card } from "@/app/_components/ui/card";
-import EasilyPosts from "@/app/_components/easilyPosts";
+import { motion } from "framer-motion";
+import { TypingEffect } from "../ui/typingEffect";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 export function HeroSection() {
+  const scrollToSection = () => {
+    const nextSection = document.getElementById("service");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="px-6 py-12 sm:py-16 md:py-24">
-      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-10 md:gap-20 lg:gap-32">
-        <div className="w-full justify-center flex md:hidden">
-          <Card className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[30rem] lg:h-[30rem]">
-            <Image
-              src="/logo(1).svg"
-              alt="Easily Beta"
-              fill
-              className="object-contain p-6"
-            />
-          </Card>
-        </div>
-        {/* 왼쪽 콘텐츠 */}
-        <div className="flex flex-col gap-8 md:gap-12 w-full">
-          <div className="text-center md:text-left">
-            <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-tight mb-6 tracking-tight">
-              세상 쉬운 영상기획
-              <br />
-              이젠 <span className="text-primary">이즐리</span>로 하세요
-            </h1>
-            <p className="text-base sm:text-lg md:text-xl text-muted-foreground mb-6 md:mb-8">
-              이즐리는 영상기획툴을 제공합니다
-            </p>
-            <Button
-              asChild
-              size="lg"
-              className="bg-primary text-white hover:bg-[#e55a1f] px-6 sm:px-8 py-3 sm:py-4 text-base sm:text-lg md:text-xl"
-            >
-              <Link href="https://easily-dashboard.jojicompany.com/dashboard/proposal/create">
-                기획안 만들기
-              </Link>
-            </Button>
-          </div>
-
-          {/* 공지사항 */}
-          <EasilyPosts />
+    <div className="w-full flex flex-col items-center text-center px-6 py-16 sm:py-24 md:py-32">
+      <div className="flex flex-col gap-6 w-full max-w-[1100px]">
+        <div className="text-white sm:text-6xl md:text-7xl font-extrabold tracking-tighter">
+          <TypingEffect text={"간편하게 만드는 영상 기획안"} />
         </div>
 
-        {/* 오른쪽 콘텐츠 */}
-        <div className="w-full justify-center hidden md:flex">
-          <Card className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[30rem] lg:h-[30rem]">
-            <Image
-              src="/logo(1).svg"
-              alt="Easily Beta"
-              fill
-              className="object-contain p-6"
-            />
-          </Card>
+        <p className=" mt-5 text-white text-lg sm:text-4xl md:text-4xl font-medium opacity-90">
+          창의적인 영상 기획을{" "}
+          <span className="text-point font-bold">
+            쉽고, 빠르게, 효과적으로!
+          </span>
+        </p>
+        <div className="flex sm:flex-row justify-center w-full gap-4 mt-6">
+          <motion.a
+            href="https://easily-dashboard.jojicompany.com/dashboard/proposal/create"
+            className="w-72 h-14 px-6 py-3 text-white bg-point rounded-lg shadow-lg text-xl font-semibold flex items-center justify-center"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            🚀 에디터로 이동
+          </motion.a>
+
+          <motion.a
+            href="/post"
+            className="w-72 h-14 px-6 py-3 text-white border-2 border-white rounded-lg text-xl font-semibold flex items-center justify-center"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            🔍 공지사항
+          </motion.a>
         </div>
+      </div>
+      <div className="relative flex justify-between mt-20">
+        <motion.div
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.7 }}
+          >
+            <IoMdCheckmarkCircleOutline className="text-point/50" />
+          </motion.span>
+          AI사용
+        </motion.div>
+
+        <motion.div
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.2 }}
+          >
+            <IoMdCheckmarkCircleOutline className="text-point/50" />
+          </motion.span>
+          고품질
+        </motion.div>
+
+        <motion.div
+          className="p-2 text-4xl sm:text-5xl md:text-3xl text-white font-bold mt-4 flex items-center gap-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
+        >
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.7 }}
+          >
+            <IoMdCheckmarkCircleOutline className="text-point/50" />
+          </motion.span>
+          편리함
+        </motion.div>
+      </div>
+      <div className="absolute flex justify-center bottom-0">
+        <motion.button
+          className="relative w-40 h-20 bg-gradient-to-b from-white to-white shadow-2xl flex items-center justify-center"
+          onClick={scrollToSection}
+          whileHover={{ scale: 1.3, y: -10 }}
+          animate={{ y: [0, -10, 0] }}
+          transition={{
+            repeat: Infinity,
+            duration: 4,
+            ease: "easeInOut",
+          }}
+          style={{
+            clipPath: "polygon(50% 100%, 0% 0%, 100% 0%)",
+            boxShadow: "0px 2px 15px rgba(0, 0, 0, 0.4)",
+          }}
+        ></motion.button>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,9 @@ export default function Home() {
       >
         <section
           id="hero"
-          className="min-h-screen flex items-center justify-center bg-gray-100"
+          className="min-h-screen flex items-center justify-center transition-all 
+             bg-gradient-to-tr from-primary to-yellow-300 
+             bg-[length:200%_200%] animate-gradient"
         >
           <HeroSection />
         </section>
