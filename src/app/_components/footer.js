e89b7fe0d96d19e6161524ddd41def01a3c28d4f
Alang Kim <rlaangh77@gmail.com>
2025-02-25 18:11:10 +0900

    feat: footer 간소화

--- src/app/_components/footer.js
+++ src/app/_components/footer.js
@@ -1,49 +1,9 @@
-import { Youtube } from "lucide-react";
-import { Instagram } from "lucide-react";
-import { COMPANY_INFO } from "../_consts/company";
-
 export default function Footer() {
   return (
-    <div className="w-full p-10 h-auto bg-black text-white">
-      <h2 className="text-2xl font-bold mb-6">{COMPANY_INFO.Name}</h2>
-      <p className="text-lg text-muted-foreground text-gray-50">
-        대표자: {COMPANY_INFO.Representative}
-        <br />
-        주소 : {COMPANY_INFO.Address}
-        <br />
-        contact mail : {COMPANY_INFO.Email}
-        <br />
-        contact tel : {COMPANY_INFO.Tel}
-      </p>
-      <br />
-      <a
-        href={COMPANY_INFO.Service}
-        className="text-lg text-muted-foreground text-gray-100"
-      >
-        서비스약관
-      </a>
-      <br />
-      <a
-        href={COMPANY_INFO.Userule}
-        className="text-lg text-muted-foreground text-gray-100"
-      >
-        개인정보이용약관
-      </a>
-      <br />
-      <div className="flex space-x-2 mt-5">
-        <a
-          href={COMPANY_INFO.Youtube}
-          className="text-lg text-muted-foreground"
-        >
-          <Youtube size={25} />
-        </a>
-        <a
-          href={COMPANY_INFO.Instagram}
-          className="text-lg text-muted-foreground"
-        >
-          <Instagram size={25} />
-        </a>
-      </div>
+    <div className="w-full px-10 py-2 h-auto bg-black text-white text-center">
+      <span className="text-sm text-gray-500 ">
+        Copyright 2025. 이즐리. All rights reserved.
+      </span>
     </div>
   );
 }
