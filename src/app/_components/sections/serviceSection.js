c486ba3031158aafbf1259048e04bd96266ab891
Alang Kim <rlaangh77@gmail.com>
2025-02-25 18:18:19 +0900

    feat: 서비스 섹션 레이아웃 스타일 개선

--- src/app/_components/sections/serviceSection.js
+++ src/app/_components/sections/serviceSection.js
@@ -5,12 +5,9 @@ import { TypographyH2, TypographyP } from "../ui/typography";
 export function ServiceSection({ ...props }) {
   return (
     <SectionLayout {...props}>
-      <div className="max-w-7xl mx-auto px-6 py-24 text-center bg-white rounded-2xl shadow-xl">
-        {SERVICE_INFOS.map((item) => (
-          <div
-            key={item.id}
-            className="relative p-10 mb-16 max-w-6xl mx-auto flex flex-col gap-4"
-          >
+      <div className="max-w-7xl px-0 md:px-6 py-16 text-center bg-white rounded-2xl shadow-xl flex flex-col gap-8">
+        {SERVICE_INFOS.map((item, index) => (
+          <div key={item.id} className="relative px-4  flex flex-col gap-4">
             <TypographyH2>{item.title}</TypographyH2>
             <div className="relative w-full aspect-video overflow-hidden rounded-lg shadow-lg">
               <video
@@ -26,6 +23,9 @@ export function ServiceSection({ ...props }) {
               className="whitespace-pre-line"
               dangerouslySetInnerHTML={{ __html: item.subtitle }}
             />
+            {index !== SERVICE_INFOS.length - 1 && (
+              <div className="w-full h-[1px] md:my-4 bg-gray-200" />
+            )}
           </div>
         ))}
       </div>
