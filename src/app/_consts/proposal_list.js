f0547122f5c7b81de2f0d09c9c628eccaf5f3812
JEONGWOON KIM ( MOOHO KIM ) <75013334+AlangGY@users.noreply.github.com>
2025-02-26 11:21:03 +0900

    feat: proposal section 스타일 개선 (#43)

--- src/app/_consts/proposal_list.js
+++ src/app/_consts/proposal_list.js
@@ -1,26 +1,18 @@
 export const PROPOSAL_EXAMPLES = [
   {
     id: 1,
-    image: "https://picsum.photos/id/237/1920/1080",
+    image: "/proposal-example/proposal-example-1.png",
   },
   {
     id: 2,
-    image: "https://picsum.photos/id/238/1920/1080",
+    image: "/proposal-example/proposal-example-2.png",
   },
   {
     id: 3,
-    image: "https://picsum.photos/id/239/1920/1080",
+    image: "/proposal-example/proposal-example-3.png",
   },
   {
     id: 4,
-    image: "https://picsum.photos/id/240/1920/1080",
-  },
-  {
-    id: 5,
-    image: "https://picsum.photos/id/241/1920/1080",
-  },
-  {
-    id: 6,
-    image: "https://picsum.photos/id/242/1920/1080",
+    image: "/proposal-example/proposal-example-4.png",
   },
 ];
