fa1e8832d87795395beaaa32299d44b28946841a
Alang Kim <rlaangh77@gmail.com>
2025-06-14 01:36:31 +0900

    feat: footer에 사업자 정보 추가

--- src/app/_components/footer.js
+++ src/app/_components/footer.js
@@ -2,8 +2,12 @@ export default function Footer() {
   return (
     <div className="w-full px-10 py-2 h-auto bg-black text-white text-center">
       <span className="text-sm text-gray-500 ">
-        Copyright 2025. 이즐리. All rights reserved.
+        Copyright 2025. 조지컴퍼니. All rights reserved.
       </span>
+      <div className="mt-1 text-xs text-gray-400">
+        <span>대표자명: 조영진&nbsp;|&nbsp;</span>
+        <span>사업자등록번호: 108-39-69341</span>
+      </div>
     </div>
   );
 }
